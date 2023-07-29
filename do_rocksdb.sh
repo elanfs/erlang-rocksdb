6534ef1fd30234908baf9fcc6d78dafd01e6068b
Eric Lan <tritium86@gmail.com>
2023-07-30 00:12:46 +0800

    Revert unused code
    - Not doing releases
    - Not using CI

--- do_rocksdb.sh
+++ do_rocksdb.sh
@@ -3,17 +3,6 @@
 set -eu
 set -x
 
-PKGNAME="$(./pkgname.sh)"
-
-if [ "${BUILD_RELEASE:-}" != 1 ] && [ -n "$PKGNAME" ]; then
-    if ./download.sh $PKGNAME; then
-        echo "done_dowloading_rocksdb"
-        exit 0
-    else
-        echo "failed to download, continue to build from source"
-    fi
-fi
-
 cd _build/cmake
 
 if type cmake3 > /dev/null 2>&1 ; then
@@ -37,23 +26,5 @@ case "$@" in
         ${CMAKE} --build . $PAR $@ || exit 1
         ;;
 esac
-
-cd ../../
-
-if [ "${BUILD_RELEASE:-}" = 1 ]; then
-    if [ -z "$PKGNAME" ]; then
-        echo "unable_to_resolve_release_package_name"
-        exit 1
-    fi
-    mkdir -p _packages
-    TARGET="_packages/${PKGNAME}"
-    gzip -c 'priv/liberocksdb.so' > "$TARGET"
-    # use openssl but not sha256sum command because in some macos env it does not exist
-    if command -v openssl; then
-        openssl dgst -sha256 "${TARGET}" | cut -d ' ' -f 2  > "${TARGET}.sha256"
-    else
-        sha256sum "${TARGET}"  | cut -d ' ' -f 1 > "${TARGET}.sha256"
-    fi
-fi
 
 echo done_building_rocksdb
